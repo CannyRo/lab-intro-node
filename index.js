cb8c41dadb039204bbafeea2655b22caf429fa60
CannyRo <whatcannydev@gmail.com>
2025-03-10 14:18:18 +0100

    Lab solved

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    // console.log("Init Items array : ",this.items);
+    this.items.push(item);
+    // console.log("After pushing : ",this.items);
+    this.items.sort((a,b) => a - b );
+    // console.log("After sorting : ",this.items);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length -1];
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((accumulator, currentValue)=>accumulator+currentValue, 0);
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    const sumOfTheArray = this.items.reduce((accumulator, currentValue)=>accumulator+currentValue, 0);
+    return sumOfTheArray / this.length;
+  }
 }
 
 module.exports = SortedList;
